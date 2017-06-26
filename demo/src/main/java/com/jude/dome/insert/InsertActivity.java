9c8eeee04fa060c9310a806d610ff9c857258dcc
dreamBigYoung <18355236154@163.com>
2017-06-26 17:58:59 +0800

    解决当前条目数为0时，删除条目操作的异常

--- demo/src/main/java/com/jude/dome/insert/InsertActivity.java
+++ demo/src/main/java/com/jude/dome/insert/InsertActivity.java
@@ -25,7 +25,7 @@ import java.util.Random;
  * Created by zhuchenxi on 2016/12/12.
  */
 
-public class InsertActivity extends AppCompatActivity{
+public class InsertActivity extends AppCompatActivity {
     private EasyRecyclerView recyclerView;
     private RecyclerArrayAdapter<Person> adapter;
 
@@ -36,38 +36,41 @@ public class InsertActivity extends AppCompatActivity{
         setContentView(R.layout.activity_recyclerview);
         recyclerView = (EasyRecyclerView) findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        DividerDecoration itemDecoration = new DividerDecoration(Color.GRAY, Util.dip2px(this,0.5f), Util.dip2px(this,72),0);
+        DividerDecoration itemDecoration = new DividerDecoration(Color.GRAY, Util.dip2px(this, 0.5f), Util.dip2px(this, 72), 0);
         itemDecoration.setDrawLastItem(false);
         recyclerView.addItemDecoration(itemDecoration);
         recyclerView.setAdapterWithProgress(adapter = new PersonAdapter(this));
         List<Person> persons = DataProvider.getPersonList(0);
-        adapter.addAll(persons.subList(0,3));
+        adapter.addAll(persons.subList(0, 3));
         recyclerView.setItemAnimator(new DefaultItemAnimator());
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_insert,menu);
+        getMenuInflater().inflate(R.menu.menu_insert, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         Random random = new Random();
-        int pos = random.nextInt(adapter.getCount());
+        int len = adapter.getCount();
+        if (len > 0) {
+            int pos = random.nextInt(len);
 //        int pos = 0;
-        List<Person> persons = DataProvider.getPersonList(0);
-        Person data = persons.get(random.nextInt(persons.size()));
-        switch (item.getItemId()){
-            case R.id.ic_add:
-                adapter.insert(data,pos);
-                break;
-            case R.id.ic_remove:
-                adapter.remove(pos);
-                break;
-            case R.id.ic_refresh:
-                adapter.update(data,pos);
-                break;
+            List<Person> persons = DataProvider.getPersonList(0);
+            Person data = persons.get(random.nextInt(persons.size()));
+            switch (item.getItemId()) {
+                case R.id.ic_add:
+                    adapter.insert(data, pos);
+                    break;
+                case R.id.ic_remove:
+                    adapter.remove(pos);
+                    break;
+                case R.id.ic_refresh:
+                    adapter.update(data, pos);
+                    break;
+            }
         }
         return true;
     }
